62ef69bc398826e1bfd60a177e041849ecff286f
EnyaGalvez <galvezenya@gmail.com>
2025-10-12 16:40:41 -0600

    loggedMenu

--- app/src/main/java/com/turis/gestiondetiempo/ui/theme/Color.kt
+++ app/src/main/java/com/turis/gestiondetiempo/ui/theme/Color.kt
@@ -2,8 +2,21 @@ package com.turis.gestiondetiempo.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
+// light mode
+val Surface = Color(0xFFF3F9F7)
+val Cyan40 = Color(0xFF006861)
+val Cyan90 = Color(0xFFB5EEE5)
+val Cyan30 = Color(0xFF004E48)
+val Green40 = Color(0xFF4C652C)
+val Green90 = Color(0xFFCBEBA1)
+val Green30 = Color(0xFF354D15)
+val Blue40 = Color(0xFF3F5E8F)
+val Blue90 = Color(0xFFD3E1FC)
+val Blue30 = Color(0xFF254579)
+val Yellow40 = Color(0xFFF8E087)
+val Yellow90 = Color(0xFF6D5C0F)
+val Yellow30 = Color(0xFF534400)
+
 val Pink80 = Color(0xFFEFB8C8)
 
 val Purple40 = Color(0xFF6650a4)
--- app/src/main/java/com/turis/gestiondetiempo/ui/theme/Theme.kt
+++ app/src/main/java/com/turis/gestiondetiempo/ui/theme/Theme.kt
@@ -10,27 +10,25 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
+import com.turis.gestiondetiempo.ui.theme.Cyan40
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Cyan40,
+    primaryContainer = Cyan90,
+    onPrimaryContainer = Cyan30
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = Cyan40,
+    primaryContainer = Cyan90,
+    onPrimaryContainer = Cyan30,
+    secondary = Green40,
+    secondaryContainer = Green90,
+    onSecondaryContainer = Green30,
+    tertiary = Blue40,
+    tertiaryContainer = Blue90,
+    onTertiaryContainer = Blue30,
 
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
 )
 
 @Composable
