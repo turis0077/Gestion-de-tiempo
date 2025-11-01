6f0b2563cea07b5d60fdd47b460946fef312dc23
JAVIERCLO <cha23132@uvg.edu.gt>
2025-10-31 19:46:25 -0600

    agregando pantallas al Nav

--- app/src/main/java/com/turis/gestiondetiempo/nav/navControllers/LoggedNav.kt
+++ app/src/main/java/com/turis/gestiondetiempo/nav/navControllers/LoggedNav.kt
@@ -1,11 +1,23 @@
 package com.turis.gestiondetiempo.nav.navControllers
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import com.turis.gestiondetiempo.features.calendar.CalendarDayView
+import com.turis.gestiondetiempo.features.calendar.CalendarMonthView
+import com.turis.gestiondetiempo.features.calendar.CalendarWeekView
 import com.turis.gestiondetiempo.nav.routes.LoggedRoutes
 
 @Composable
@@ -33,7 +45,41 @@ private fun LoggedNav(onLoggedIn: () -> Unit) {
         }
 
         composable<LoggedRoutes.Calendar> {
+            var selectedTabIndex by remember { mutableStateOf(0) }
 
+            Column {
+                // Barra de pestañas
+                TabRow(
+                    selectedTabIndex = selectedTabIndex,
+                    modifier = Modifier.padding(bottom = 8.dp),
+                ) {
+                    Tab(
+                        selected = selectedTabIndex == 0,
+                        onClick = { selectedTabIndex = 0 }
+                    ) {
+                        Text("Mes", modifier = Modifier.padding(16.dp))
+                    }
+                    Tab(
+                        selected = selectedTabIndex == 1,
+                        onClick = { selectedTabIndex = 1 }
+                    ) {
+                        Text("Semana", modifier = Modifier.padding(16.dp))
+                    }
+                    Tab(
+                        selected = selectedTabIndex == 2,
+                        onClick = { selectedTabIndex = 2 }
+                    ) {
+                        Text("Día", modifier = Modifier.padding(16.dp))
+                    }
+                }
+
+                //
+                when (selectedTabIndex) {
+                    0 -> CalendarMonthView()
+                    1 -> CalendarWeekView()
+                    2 -> CalendarDayView()
+                }
+            }
         }
 
         composable<LoggedRoutes.Profile> {
