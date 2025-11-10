0d62a4cf71d94fdb71a4cfdc33c401b0deef93d5
JAVIERCLO <cha23132@uvg.edu.gt>
2025-11-10 14:14:21 -0600

    agregando carpetas de data, versiones, dependencias y bundles para ktor, y permiso de internet en android manifest

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,4 +60,8 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+    implementation(libs.bundles.ktor)
+    implementation(libs.kotlinx.serialization.json)
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.kotlinx.coroutines.android)
 }
